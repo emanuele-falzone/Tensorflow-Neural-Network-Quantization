eb70b8001a0e959623940e44203798f0895141fa
Emanuele Falzone <emafalzo@gmail.com>
2018-05-16 20:24:18 +0200

    Improve inference script

--- inference.py
+++ inference.py
@@ -14,52 +14,45 @@ def load_graph(model_file):
 
 	return graph
 
-if __name__ == "__main__":
-	file_name = ""
-	model_file = ""
+def load_image(file_name):
+	file_reader = tf.read_file(file_name, "file_reader")
 
+	image_reader = tf.image.decode_png(file_reader, channels = 1, name='png_reader')
+
+	float_caster = tf.cast(image_reader, tf.float32)
+	dims_expander = tf.expand_dims(float_caster, 0);
+	resized = tf.image.resize_bilinear(dims_expander, [28, 28])
+	normalized = tf.divide(tf.subtract(resized, [0]), [255])
+
+	sess = tf.Session()
+	return sess.run(normalized)
+
+def predict(graph, image):
+	input_operation = graph.get_operation_by_name("import/Placeholder");
+	output_operation = graph.get_operation_by_name("import/final_result");
+
+	with tf.Session(graph=graph) as sess:
+		start = time.time()
+		results = sess.run(output_operation.outputs[0],
+			  {input_operation.outputs[0]: image})
+		end=time.time()
+	results = np.squeeze(results)
+	return results, end-start
+
+if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--image", help="image to be processed")
 	parser.add_argument("--graph", help="graph/model to be executed")
 	args = parser.parse_args()
 
-	if args.graph:
-		model_file = args.graph
-	if args.image:
-		file_name = args.image
-	
 	if args.graph and args.image:
 
-		input_name = "file_reader"
-		output_name = "normalized"
+		image = load_image(args.image)
+		graph = load_graph(args.graph)
 
-		file_reader = tf.read_file(file_name, input_name)
+		results, prediction_time = predict(graph, image)
 
-		image_reader = tf.image.decode_png(file_reader, channels = 1, name='png_reader')
-
-		float_caster = tf.cast(image_reader, tf.float32)
-		dims_expander = tf.expand_dims(float_caster, 0);
-		resized = tf.image.resize_bilinear(dims_expander, [28, 28])
-		normalized = tf.divide(tf.subtract(resized, [0]), [255])
-		sess = tf.Session()
-		t = sess.run(normalized)
-
-		graph = load_graph(model_file)
-
-		input_name = "import/Placeholder"
-		output_name = "import/final_result"
-
-		input_operation = graph.get_operation_by_name(input_name);
-		output_operation = graph.get_operation_by_name(output_name);
-
-		with tf.Session(graph=graph) as sess:
-			start = time.time()
-			results = sess.run(output_operation.outputs[0],
-				  {input_operation.outputs[0]: t})
-			end=time.time()
-		results = np.squeeze(results)
-
-		print('Evaluation time: {:.9f}s\n'.format(end-start))
+		print('Evaluation time: {:.9f}s\n'.format(prediction_time))
 
 		top_k = results.argsort()[-5:][::-1]
 
@@ -67,8 +60,8 @@ if __name__ == "__main__":
 			print(i, results[i])
 
 	else:
-		print("ERROR")
-		
+		parser.print_help()
+
 
 
 
